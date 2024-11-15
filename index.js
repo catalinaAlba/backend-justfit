ff16954b6cbfec9bd56c39da2d6b5b18157f64cf
Cata <127821154+catalinaAlba@users.noreply.github.com>
2024-11-15 17:26:13 -0300

    agregado: modeloUsuario, y control de sesion, login y logout

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { conectarDB } from "./database/connection.js";
-import { getBarritas } from "./controllers/getBarritas.js";
-import { getBarritaById } from "./controllers/getBarritaById.js";
 import { mostrarDataRequest } from "./middlewares/mostrarDataRequest.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
+
+// Barritas
+import { getBarritas } from "./controllers/getBarritas.js";
+import { getBarritaById } from "./controllers/getBarritaById.js";
+
+//Usuario
+import { postUsuario } from "./controllers/postUsuario.js";
+import { loginUsuario } from "./controllers/loginUsuario.js";
+import { controlarSesion } from "./middlewares/controlarSession.js";
+import { logoutUsuario } from "./controllers/logoutUsuario.js";
+
 
 const app = express();
 const port = 3000;
@@ -17,9 +26,17 @@ await conectarDB();
 app.use(mostrarDataRequest)
 
 // llamada a la api
-app.get("/", (req, res) => {res.send("API Just Fit")})
+app.get("/", (req, res) => { res.send("API Just Fit") })
 
-//endpoints:
+//endpoints login:
+app.post("/registrar", postUsuario)
+app.post("/login", loginUsuario)
+
+// middleware control de sesion valida
+app.use(controlarSesion)
+app.post("/logout", logoutUsuario)
+
+//endpoints barritas:
 app.get("/barritas", getBarritas)
 app.get("/barrita/:id", getBarritaById)
 
@@ -27,4 +44,4 @@ app.get("/barrita/:id", getBarritaById)
 app.use(errorHandler)
 
 //puerto
-app.listen(port, () => {console.log(`Servidor corriendo en puerto ${port}`);})
+app.listen(port, () => { console.log(`Servidor corriendo en puerto ${port}`); })
